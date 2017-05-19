027c20bdb3a1c43817389f993877192f703a8f8d
Henri <sternhenri2@gmail.com>
2017-05-19 18:49:46 +0000

    update

--- src/dnsmasq.sh
+++ src/dnsmasq.sh
@@ -4,7 +4,8 @@ Log=/root/g8keepr/log/dnsmasq.log
 Script=/root/g8keepr/src/detect_connection.py
 # env not set here (check env >> $debug, so need to remediate that)
 # need to export /tmp/usr/lib to get access to python
-export LD_LIBRARY_PATH=:/tmp/usr/lib/:/tmp/usr/lib/:/tmp/usr/lib/
+export PATH=$PATH:/tmp/usr/bin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/usr/lib/
 date >> $Log
 echo $* >> $Log
 $Script $* 
