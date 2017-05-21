3c8be7f424b0962f70681d1da264ee35342a1b29
Yan Michalevsky <ymcrcat@gmail.com>
2017-05-21 03:08:28 +0000

    Added ndsutils.py

---
+++ src/ndsutils.py
@@ -0,0 +1,29 @@
+#!/tmp/usr/bin/python
+import os
+
+CLIENT_PARAMS = ('client_id','ip','mac','added','active','duration','token','state','downloaded','avg_down_speed','uploaded','avg_up_speed')
+
+def ndsread(ndsresponse):
+	return ndsresponse.readline().split('=')[1]
+
+def read_client_status(ndsresponse):
+	client_status = {}
+	for param in CLIENT_PARAMS:
+		client_status[param] = ndsread(ndsresponse)
+	ndsresponse.readline()
+	print client_status
+
+def get_clients():
+	ndsresponse = os.popen('ndsctl clients')
+	num_clients = int(ndsresponse.readline())
+	print num_clients
+	ndsresponse.readline()
+	
+	for i in xrange(num_clients):
+		read_client_status(ndsresponse)
+
+def main():
+	get_clients()
+
+if __name__ == '__main__':
+	main()
