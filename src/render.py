216c33bf25d7ef651139e4299e641b9b03247adc
Yan Michalevsky <yanm2@cs.stanford.edu>
2017-05-21 02:16:14 -0700

    Render.py takes devices_json as argument

--- src/render.py
+++ src/render.py
@@ -6,16 +6,18 @@ import datetime
 import json
 
 def main():
-	template = Template(io.open(sys.argv[1],'r', encoding="utf-8").read())
+	template_file, devices_json, output_file = sys.argv[1:]
 
-	devices = json.load(open('devices.json','r'))
+	template = Template(io.open(template_file, 'r', encoding="utf-8").read())
+
+	devices = json.load(open(devices_json,'r'))
 	print devices
 
 	rendered = template.render(devices=devices, 
 														 updatetime=datetime.datetime.now().ctime(),
 														 notifications_count=0)
 	
-	with io.open(sys.argv[2], 'w', encoding='utf-8') as output:
+	with io.open(output_file, 'w', encoding='utf-8') as output:
 		output.write(rendered)
 
 if __name__ == '__main__':
