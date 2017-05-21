1debfdd9a364df7d630302741748ded8e352f1b9
Yan Michalevsky <yanm2@cs.stanford.edu>
2017-05-21 04:19:38 -0700

    Simple HTTP server to handle rendering with parameters

--- src/render.py
+++ src/render.py
@@ -5,23 +5,24 @@ from jinja2 import Template
 import datetime
 import json
 
-def render(template_file, devices_json, output_file):
+def render(template_file, devices_json, authtarget, output):
 	template = Template(io.open(template_file, 'r', encoding="utf-8").read())
 
 	devices = json.load(open(devices_json,'r'))
-	print devices
+	# print devices
 
-	rendered = template.render(devices=devices, 
-														 updatetime=datetime.datetime.now().ctime(),
-														 notifications_count=0)
-	
-	with io.open(output_file, 'w', encoding='utf-8') as output:
-		output.write(rendered)
+	stream = template.stream(devices=devices, 
+													 updatetime=datetime.datetime.now().ctime(),
+													 notifications_count=0,
+													 authtarget=authtarget)
+	stream.dump(output, encoding='utf-8')
 
 
 def main():
 	template_file, devices_json, output_file = sys.argv[1:]
-	render(template_file, devices_json, output_file)
+
+	with open(output_file, 'w') as output:
+		render(template_file, devices_json, authtarget='#', output)
 
 if __name__ == '__main__':
 	main()
