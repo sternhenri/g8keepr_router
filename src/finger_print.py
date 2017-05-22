ba5cf16fef3f87f752296555e98ed9cbe32a958b
tianyuan186 <tianyuan186@gmail.com>
2017-05-21 21:54:00 -0700

    update to clog and add device name as a parameter

--- src/finger_print.py
+++ src/finger_print.py
@@ -2,18 +2,20 @@ import sys
 import requests
 import socket
 import nmap
+from detect_connection import *
 
 
 
-def finger_print(ip, mac):
+def finger_print(name,ip, mac):
 
     flag = 1
 
-    print ("Start the finger printing ...")
-
+    cLog ("Start the finger printing ...")
+    if name != '':
+        return name
 
     # whether it has a webinterface
-    print ("Finger printing the ip address ...")
+    cLog ("Finger printing the ip address ...")
     #try:
     #    socket.gethostbyaddr(ip)
     #except socket.herror:
@@ -22,9 +24,9 @@ def finger_print(ip, mac):
 
 
     r = requests.get('http://'+ ip)
-    if r.status_code == 401: 
-        print (r.text)
-        print ("Identify the features of the device's web interface. Match 50 devices.")
+    if r.status_code == 401:
+        cLog (r.text)
+        cLog ("Identify the features of the device's web interface. Match 50 devices.")
     else:
         flag = 0
 
@@ -32,11 +34,12 @@ def finger_print(ip, mac):
 
     print ("Finger printing the mac address ...")
     if mac == '00:8f:bd:ae:a3:ec':
-        print ("Mac address matched. Match 300 devices.")
+        cLog ("Mac address matched. Match 300 devices.")
+        print("text")
     else:
         flag = 0
 
-    print ("Finger printing the open ports ...")
+    cLog ("Finger printing the open ports ...")
 
     targetHost = ip
     targetPorts = '1-100'
@@ -45,21 +48,24 @@ def finger_print(ip, mac):
         scanner = nmap.PortScanner()
         scanner.scan(targetHost, targetPorts)
         for targetPort in scanner[targetHost]['tcp']:
-            print str(targetHost) + ':' + str(targetPort) + 'is up'
+            cLog (str(targetHost) + ':' + str(targetPort) + 'is up')
             upPorts.append(targetPort)
     except Exception, e:
-        print '[-] Something bad happened during the scan: ' + str(e)
+        cLog ('[-] Something bad happened during the scan: ' + str(e))
+      
     # whether it uses digist authetication
-    print ("Finger printing the authetication ...")
+    cLog ("Finger printing the authetication ...")
     if upPorts == [80, 23] :
-        print 'Identify the ports that are up. Match 10 devices.'
+        cLog ('Identify the ports that are up. Match 10 devices.')
     else:
         flag = 0
 
     # return the device name
     if flag == 1:
-        print ("Device identified! This device ( IP: " + str(ip) + ", MAC: " + str(mac) + " is a P2PCamera")
+        cLog ("Device identified! This device ( IP: " + str(ip) + ", MAC: " + str(mac) + " is a P2PCamera")
         return "P2PCamera"
     return "unknown";
 
-#finger_print('192.168.1.232', '00:8f:bd:ae:a3:ec')
+#result = finger_print( '','192.168.1.232', '00:8f:bd:ae:a3:ec')
+#print result
+
