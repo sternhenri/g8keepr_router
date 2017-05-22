433c06f8e3eb15d8f43b0d22941a7411d6f2a10e
tianyuan186 <tianyuan186@gmail.com>
2017-05-21 22:11:35 -0700

    export log, edit testing script

---
+++ src/log.py
@@ -0,0 +1,13 @@
+CUSTOM_LOG = '/root/g8keepr/log/events.log'
+def log(string, path):
+    with open(path, 'ab') as logfile:
+        now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        logfile.write(now)
+        logfile.write(" -- ")
+        logfile.write(string)
+        logfile.write("\n")
+    if DEBUG:
+        print string
+
+def cLog(string):
+    log(string, CUSTOM_LOG)
