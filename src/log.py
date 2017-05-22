19c450b3aa3e077b90dfa6f84b8085dc0c9881e8
t <buenz@cs.stanford.edu>
2017-05-21 22:47:09 -0700

    Fixing Log

--- src/log.py
+++ src/log.py
@@ -1,4 +1,6 @@
+from time import gmtime, strftime
 CUSTOM_LOG = '/root/g8keepr/log/events.log'
+DEBUG = True
 def log(string, path):
     with open(path, 'ab') as logfile:
         now = strftime("%Y-%m-%d %H:%M:%S", gmtime())
